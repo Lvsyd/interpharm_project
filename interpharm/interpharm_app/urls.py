f92f014772fe75f1df7e71624417f243834e1e02
Lvsyd <fmarek31@gmail.com>
2022-03-07 10:38:40 +0100

    added pages

--- interpharm/interpharm_app/urls.py
+++ interpharm/interpharm_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('domov', views.index, name='domov'),
     path('o-nas', views.o_nas, name='o-nas'),
+    path('kontakt', views.kontakt, name='kontakt'),
+    path('sluzby', views.sluzby, name='sluzby'),
+    path('zakaznicka-zona', views.zakaznicka_zona, name='zakaznicka-zona'),
+    path('zastupujuce-znacky', views.zastupujuce_znacky, name='zastupujuce-znacky'),
 ]
--- interpharm/interpharm_app/views.py
+++ interpharm/interpharm_app/views.py
@@ -14,3 +14,26 @@ def o_nas(request):
     template = 'o_nas.html'
     return render(request, template, context)
 
+
+def kontakt(request):
+    context = {}
+    template = 'kontakt.html'
+    return render(request, template, context)
+
+
+def sluzby(request):
+    context = {}
+    template = 'sluzby.html'
+    return render(request, template, context)
+
+
+def zakaznicka_zona(request):
+    context = {}
+    template = 'zakaznicka_zona.html'
+    return render(request, template, context)
+
+
+def zastupujuce_znacky(request):
+    context = {}
+    template = 'zastupujuce-znacky.html'
+    return render(request, template, context)
